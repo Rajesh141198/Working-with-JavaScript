f01bfe4ce3818bf9b8fbc9c823d296e704fddf23
Chintu <rajeshboddu2212@gmail.com>
2024-07-19 19:42:44 -0500

    Displays Timer

---
+++ propapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Mycomponent from './Mycomponent';
+
+function App() {
+  return (
+    <div className="App">
+     <Mycomponent name = "Front End Developer"/> 
+    </div>
+  );
+}
+
+export default App;
---
+++ propapp/src/Mycomponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const proptest = ({name}) => {
+
+    return(
+      <center>
+           <h1>Hello {name}!, How are you doing.</h1>
+      </center>
+          
+    ) ;
+    
+};
+
+
+  
+  export default proptest;
